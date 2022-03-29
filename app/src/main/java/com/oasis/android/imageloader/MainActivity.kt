712a0b6c264d9018dfcd3fd863888e26c81d3385
oasis2008 <yaoxiaochang@17guagua.com>
2022-03-29 14:53:44 +0800

    rename module

---
+++ app/src/main/java/com/oasis/android/imageloader/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.oasis.android.imageloader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity:AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
